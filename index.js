45f0a01b4780e10f0aa3f42303b2b3c56085b5ad
Samuel Cornet <samuel@noukod.com>
2019-02-01 16:41:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your solution in this file!
-var customerName = 'bob';
-
-customerName.toUpperCase();
+// var customerName = 'bob';
+// 
+// customerName.toUpperCase();
 
 function myFunc () {
-  return 'Bob';
+  return 'bob';
+}
